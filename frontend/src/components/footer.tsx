7b526ddee10a3e11525cf7d46217e1e08aa34556
max_on <maks_on_max@mail.ru>
2024-08-26 21:22:55 +0700

    add: танцующая кнопка

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <div className="absolute w-full h-14 bg-gradient-to-r from-purple-500 to-pink-500">
+        <div className="fixed bottom-0 w-full h-14 bg-gradient-to-r from-purple-500 to-pink-500">
             <a href="https://t.me/maks_on_max">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/2048px-Telegram_logo.svg.png"
                     className="w-12 h-12 mt-1 ml-1"/></a>
--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -1,6 +1,12 @@
 const Home = () => {
     return (
-      <h1>Main</h1>
+      <div className="flex 
+            flex-col 
+            items-center 
+            place-content-center 
+            h-dvh ">
+        <button className="animate-spin w-1/6 bg-gradient-to-r from-violet-500 to-fuchsia-500">Main</button>
+      </div>
     )
   }
   
